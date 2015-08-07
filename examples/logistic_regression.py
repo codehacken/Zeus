5c654d6bc2ede1f69949a70ff8f19feee5b82a77
Codehacken <codehacken@gmail.com>
2015-08-07 14:25:11 -0400

    Comments

--- examples/logistic_regression.py
+++ examples/logistic_regression.py
@@ -3,15 +3,14 @@
 
 #!/usr/bin/python
 
-import numpy as np
-import theano as th  # This is the theano library.
-import theano.tensor as T  # Tensor from theano.
-from numpy import random as rn  # Random number generator.
+import theano as th                             # This is the theano library.
+import theano.tensor as T                       # Tensor from theano.
+from numpy import random as rn                  # Random number generator.
 
 
 # Define the inputs.
-N = 400  # This is the number of input data points.
-feats = 784  # This is the number of the features / data point.
+N = 400                                         # This is the number of input data points.
+feats = 784                                     # This is the number of the features / data point.
 
 # Generate the basic input training data set.
 # The low is 0 and high is 2 but not included.
