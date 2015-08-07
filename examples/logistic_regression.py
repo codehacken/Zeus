b2191b72cf2e674d8d91e01ac01c6de2172847bd
Codehacken <codehacken@gmail.com>
2015-08-07 14:19:07 -0400

    Simple Logistic regression function

--- examples/logistic_regression.py
+++ examples/logistic_regression.py
@@ -4,9 +4,9 @@
 #!/usr/bin/python
 
 import numpy as np
-import theano as th # This is the theano library.
-import theano.tensor as T # Tensor from theano.
-from numpy import random as rn # Random number generator.
+import theano as th  # This is the theano library.
+import theano.tensor as T  # Tensor from theano.
+from numpy import random as rn  # Random number generator.
 
 
 # Define the inputs.
@@ -19,9 +19,36 @@ D = (rn.randn(N, feats), rn.randint(size=N, low=0, high=2))
 training_steps = 10000
 
 # Construct the graph.
-# Declare all the theano variables.
+# Declare all the THEANO variables.
+x = T.dmatrix('x')
+y = T.vector('y')
 
+# Initialize the weight vectors.
+w = th.shared(rn.randn(feats), name="w")
+b = th.shared(0., name="b")
 
-print D
+# Construct Theano expression graph
+p_1 = 1 / (1 + T.exp(-T.dot(x, w) - b))         # Probability that target = 1
+prediction = p_1 > 0.5                          # The prediction thresholded.
+xent = -y * T.log(p_1) - (1-y) * T.log(1-p_1)   # Cross-entropy loss function
+cost = xent.mean() + 0.01 * (w ** 2).sum()      # The cost to minimize
+gw, gb = T.grad(cost, [w, b])                   # Compute the gradient of the cost
+                                                # (we shall return to this in a
+                                                # following section of this tutorial)
 
+# Compile
+train = th.function(
+    inputs=[x, y],
+    outputs=[prediction, xent],
+    updates=((w, w - 0.1 * gw), (b, b - 0.1 * gb)))
+predict = th.function(inputs=[x], outputs=prediction)
+
+# Train
+for i in range(training_steps):
+    pred, err = train(D[0], D[1])
+
+print "Final model:"
+print w.get_value(), b.get_value()
+print "target values for D:", D[1]
+print "prediction on D:", predict(D[0])
 
