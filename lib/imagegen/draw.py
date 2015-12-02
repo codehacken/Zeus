22ee0dc0e6568e008f5a69d5c5ec707a976fbdba
Codehacken <codehacken@gmail.com>
2015-12-01 20:42:19 -0500

    Update .gitignore for ignore PNG files generated.

--- lib/imagegen/draw.py
+++ lib/imagegen/draw.py
@@ -6,6 +6,20 @@ jpegs with various shapes and stacks of shapes.
 from PIL import Image, ImageFont, ImageDraw
 from lib.imagegen.shape import Square, Circle, Triangle
 
+# Functions to generate images.
+def gen_one_shape_image(image_size, max_shape_size, b_color, fill_color, outline_color):
+    # Create the Image.
+    im = ImageCreate(image_size, 1, background_color=b_color)
+    
+    # Generate squares.
+    for i in range(0, image_size[0], max_shape_size):
+        for j in range(0, image_size[1], max_shape_size):
+            sq = Square((100, 200), 100, fill_color='black', outline_color='black')
+            im.create_new_image()
+            im.dr_add(sq)
+    
+    
+    
 # Draw various kinds of images.
 # Creates the images.
 class ImageCreate(object):
@@ -15,7 +29,7 @@ class ImageCreate(object):
     
     # pic_size is a tuple of size 2 (containing the NxN number of pixels).
     def __init__(self, pic_size, num_shapes, **kwargs):
-        assert pic_size is tuple, "pic_size is a tuple of size 2 (NxN)"
+        assert pic_size is not tuple, "pic_size is a tuple of size 2 (NxN)"
         self.pic_size = pic_size
         self.num_shapes = num_shapes
         
@@ -31,21 +45,21 @@ class ImageCreate(object):
         
     # Helper functions.
     # Get image.
-    def get_image():
+    def get_image(self):
         return self._im
     
     # Create a new image.
     def create_new_image(self):
         self._im = Image.new('RGB', self.pic_size, self.bcolor)
-        self._dr = ImageDraw.Draw(im)
+        self._dr = ImageDraw.Draw(self._im)
         
         # List of shapes that the ImageDrawer takes.
         self._shape_list = []
     
     # Add shapes to the dr.
-    def dr_add(shape):
+    def dr_add(self, shape):
         # Checking if the input is of some type of a shape.
-        for a_shape in shape_list:
+        for a_shape in ImageCreate.shape_list:
             assert shape is not a_shape, "object should be of type " + a_shape
         
         shape.dr_add(self._dr)
